b73b7889d9efc85e14291869807b9592e7f734b1
jcmokako <124041133+jcmokako@users.noreply.github.com>
2023-01-31 11:35:15 +0100

    Update getaround_dashboard.py

--- getaround_dashboard.py
+++ getaround_dashboard.py
@@ -40,18 +40,7 @@ def func_filter(cat_column,selectbox):
     res = df[cat_column] == selectbox
     if selectbox == 'all':
         res = True
-#def func_threshold(operation in(>,<,==)):
-#        return threshold_less_0 = df[df['delay_at_checkout_in_minutes'] operation 0]
-                   
-                   
-#        for col in pd.unique(df[cat_column]):
-#            res = res & (df[cat_column] == col)
-    return res
-
-#def func_metric(filter,column):
-#    df = get_data()
-#    df[filter][columns].
-    
+        
 df = get_data()
 df_source = pd.read_csv(f"{mypath}src/getaround_delay_filled.csv")
 df_source_exc = pd.read_csv(f"{mypath}src/get_around_pricing_project_cleaned.csv")
@@ -92,12 +81,6 @@ df_time_delta_slider = df[df["time_delta_with_previous_rental_in_minutes"] == ti
 
 # near real-time / live feed simulation
 for seconds in range(200):
-
-    #df_delay_filter["age_new"] = df_delay_filter["age"] * np.random.choice(range(1, 5))
-    #df_delay_filter["balance_new"] = df_delay_filter["balance"] * np.random.choice(range(1, 5))
-    
-## set filter when 'all' is selected in a selextbox    
-
 
     # creating KPIs
     df_less = df[(df['delay_at_checkout_in_minutes']<delay_slider)&(func_filter("state",state_filter))&(func_filter("checkin_type",checkin_filter))]
@@ -155,31 +138,15 @@ for seconds in range(200):
         st.write("")
         st.write(f"State: {state_filter}, -------------------------------------------------checkin_type: {checkin_filter} ------------------------------------------------delay : {delay_slider}")
         dec,state,chec,other = st.columns([1,1,1,1])
-        #dec.metric(label="state and checkin",label_visibility='hidden',value='')
-        #dec.metric(label="state and checkin",label_visibility='hidden',value='')
-        #state.metric(label='count',value=df_col['state'].count())
-        #chec.metric(label='percentage',value=f"{round(100*df_col['state'].count()/df['state'].count(),2)}%")
-        
         state.metric(label="average price per minute",value=f'{price_per_minute} $')
         chec.metric(label="average cost per check-out",value=f"{round(price_per_minute *positive_delay_0.sum()/df['delay_at_checkout_in_minutes'].count(),2)} $")
         other.metric(label="average cost per delayed check-out",value=f'{round(price_per_minute *positive_delay_0.sum()/positive_delay_0.count(),2)} $')
-#positive_delay_0.sum()/df_source_exc['delay_at_checkout_in_minutes'].count()        
         
     with chartholder.container():
         st.subheader('Charts')
+        
         # create two columns for charts
         fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(
-                data_frame=df_state_filter, y="delay_at_checkout_in_minutes", x="checkin_type"
-            )
-            st.write(fig)
-            
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame=df_checkin_filter, x="delay_at_checkout_in_minutes")
-            st.write(fig2)
             
         with fig_col1:
             st.line_chart(negative_delay_0)
@@ -198,9 +165,9 @@ for seconds in range(200):
         
         #count=0
         ## Run the below code if the check is checked
-        #if st.checkbst.line_chart(ox(label='Detailed Data View',key=f"raw_data{count}"):
-        #    st.markdown("### Detailed Data View")
-        #    #st.write(df_source)
-        #    st.dataframe(df_source)
+        if st.checkbst.line_chart(ox(label='Detailed Data View',key="raw_data"):
+            st.markdown("### Detailed Data View")
+            st.write(df_source)
+            #st.dataframe(df_source)
         #    count +=1
         time.sleep(1)
